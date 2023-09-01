fd26def350c6386da96ec858e01b74f93d4982c1
cameron turner <17177161+cturner8@users.noreply.github.com>
2023-09-01 17:33:49 +0100

    add initial note create form

---
+++ PasswordManager/Models/VaultLoginModel.cs
@@ -0,0 +1,13 @@
+﻿namespace PasswordManager.Models;
+
+public class VaultLoginModel
+{
+    public required string Name { get; set; }
+    public string Description { get; set; }
+    public required string URL { get; set; }
+    public required string Email { get; set; }
+    public string Username { get; set; }
+    public required string Password { get; set; }
+    public string Notes { get; set; }
+    public string Category { get; set; }
+}
